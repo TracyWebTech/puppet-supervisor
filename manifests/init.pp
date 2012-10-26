92b0429d70b55d8b7dd6b9d6839c90c683ab2dcc
Davi Campos <arlocm@gmail.com>
2012-10-26 13:49:21 -0200

    Importing source

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+class supervisor {
+  package { "supervisor":
+    ensure => installed,
+  }
+
+  service { "supervisor":
+    ensure => running,
+    enable => true,
+    require => Package['supervisor'],
+    stop => '/etc/init.d/supervisord stop',
+    start => '/etc/init.d/supervisord start',
+    restart => 'supervisorctl update',
+  }
+
+  file { '/etc/supervisor/conf.d/':
+    ensure => directory,
+    recurse => true,
+    purge => true,
+    notify => Service['supervisor'],
+    require => Package['supervisor']
+  }
+}
